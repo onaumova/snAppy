71f5dd94e18487e1afb2973048714b858a8a4174
courtneywkwong <cwkwong95@gmail.com>
2019-09-28 09:22:47 -0400

    npm i d3 types

--- nimble/src/views/components/d3components/Visualization.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useRef } from 'react';
-
-export default function Visualization() {
-  const refElement = useRef(null);
-
-  return (
-    <div id='vis-container' ref={refElement} />
-  );
-}
